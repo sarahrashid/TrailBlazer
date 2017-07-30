cae9776ca784b6b714b9078381477aa66340a274
sarahrashid <30396454+sarahrashid@users.noreply.github.com>
2017-07-30 16:50:41 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Trail Blazer is a website build by Nicole Wilkonson and Sarah Rashid, two Stanford SPCS students. 
---
+++ trailblazer.js
@@ -0,0 +1,243 @@
+var express = require('express');
+var app = express();
+var formidable = require('formidable');
+var credentials = require('./credentials.js');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+var parseurl = require('parseurl');
+var mongodb = require('mongodb');
+
+// Block the header from containing information about the server
+app.disable('x-powered-by');
+// Set up Handlebars
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+//initialize session
+app.use(session({
+	// Only save back to the session store if a change was made
+	resave: false,
+	// Doesn't store data if a session is new and hasn't been modified
+	saveUninitialized: true,
+	// The secret string used to sign the session id cookie
+	secret: credentials.cookieSecret,
+}));
+
+
+// Create a directory called public and then a directory named img inside of it app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+//The routes go here!!!
+
+app.get('/', function(req, res){
+	// Point at the home.handlebars view, 1st action is render Home page
+	res.render('home');
+});
+
+app.get('/signup', function(req, res){
+	res.render('signup');
+});
+
+app.get('/register', function(req,res) {
+	res.render('register');
+});
+
+app.get('/postTrail', function(req,res) {
+	res.render('postTrail');
+});
+
+//connect SignUp page and data to MongoDB
+app.post('/register', function(req,res) {
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/trailblazer';
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Unable to connect to the Server:', err);
+		}
+		else {
+			console.log('Connected to server');
+			var collection = db.collection('registration');
+			var signup = {
+				firstname: req.body.firstname,
+				lastname: req.body.lastname, username: req.body.username,
+				email: req.body.email, pwd: req.body.pwd, gender: req.body.gender
+			};
+			collection.insert([signup], function (err, result) {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					res.redirect("/")
+				}
+				db.close();
+			});
+		}
+	});
+});
+
+app.post('/postTrail', function (req, res) {
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/trailblazer';
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Cannot post to database', err);
+		}
+		else {
+			console.log('Posted to database');
+			var collection = db.collection('trails');
+			var postTrail = { 
+				imageUrl: req.body.picture,
+				trailLocation: req.body.trailLocation,
+				trailDescription: req.body.trailDescription,
+				trailReccomendation: req.body.trailReccomendation
+			};
+			collection.insert([postTrail], function (err, result) {
+				if (err){
+					console.log(err);
+				}
+				else {
+					res.redirect("/explorehikes")
+				}
+				db.close();
+			});
+		}
+	});
+}); 
+
+
+
+app.get('/beginnertips', function(req,res){
+	res.render('beginnertips');
+});
+
+app.get('/featuredhike',function(req,res){
+	res.render('featuredhike');
+});
+
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
+app.get('/explorehikes', function(req,res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/trailblazer';
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Cannot post to database', err);
+		}
+		else {
+			console.log('Retrieving all trails from database');
+			var collection = db.collection('trails');
+			collection.find().toArray(function(err, result) {
+				if (err){
+					console.log(err);
+				}
+				else {
+					
+					res.render('explorehikes', {trails: result});
+				}
+				db.close();
+			});
+		}
+	});
+});
+
+app.get('/login',function(req,res){
+	res.render('login');
+});
+
+app.get('/checklogin',function(req,res){
+	res.render('checklogin');
+});
+
+app.post('/checklogin', function(req,res) {
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/trailblazer';
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Unable to connect to the Server:', err);
+		}
+		else {
+			console.log('Connected to server');
+			var collection = db.collection('registration');
+			var a = req.body.username; 
+			var b = req.body.password; 
+			collection.find({"username": a, "password":b}).toArray(function (err, result) {
+				if (err) {
+					res.send(err);
+				} else if (result.length) {
+					res.redirect("/myprofile");
+				} else {
+					res.send('Invalid login');
+				}
+				db.close();
+			}); 
+		} 
+	}); 
+});
+
+app.get('/myprofile',function(req,res){
+	res.render('myprofile');
+});
+
+app.use(function(req, res, next){
+	console.log('Looking for URL : ' + req.url);
+	next();
+});
+
+//Session function
+app.use(function(req, res, next){
+	var views = req.session.views;
+	// If no views initialize an empty array
+	if(!views){
+		views = req.session.views = {};
+	}
+	// Get the current path
+	var pathname = parseurl(req).pathname;
+	// Increment the value in the array using the path as the key
+	views[pathname] = (views[pathname] || 0) + 1;
+	next();
+});
+
+// You can also report and throw errors
+app.get('/junk', function(req, res, next){
+	console.log('Tried to access /junk');
+	throw new Error('/junk does\'t exist');
+});
+
+// Catches the error and logs it and then continues down the pipeline 
+app.use(function(err, req, res, next){
+	console.log('Error : ' + err.message);
+	next();
+});
+
+//Custom 404 Page
+app.use(function(req, res) {
+	res.type('text/html');
+	res.status(404);
+	// Point at the 404.handlebars view
+	res.render('404');
+});
+
+// Custom 500 Page
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	// Point at the 500.handlebars view
+	res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate');
+});
+
+
